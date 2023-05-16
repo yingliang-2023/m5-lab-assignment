ae3f3e9d02bfc18aa8efffd19c6a143dd7234830
YING LIANG <y.liang.168@westcliff.edu>
2023-05-15 22:41:11 -0500

    addCartPage

---
+++ src/Cart.js
@@ -0,0 +1,32 @@
+import './App.css';
+import React, { Component } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {faShoppingCart} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import products from "./products";
+import Product from "./displayProduct";
+
+
+class Cart extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+       products:products
+      };
+    };
+
+    render() {
+        return (
+            <div>
+                {this.state.products.map((product)=>
+                    <Product key={product.key}
+                    desc={product.desc}
+                    image={product.image}
+                    rating={product.rating}
+                    value={product.value} />
+                    )}
+            </div>
+        )}
+}
+
+export default Cart;
