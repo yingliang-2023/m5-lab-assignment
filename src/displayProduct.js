e6413e387fb38806ddee76b91864e21e36c8f49c
YING LIANG <y.liang.168@westcliff.edu>
2023-05-31 22:01:47 -0500

    fixed modal

--- src/displayProduct.js
+++ src/displayProduct.js
@@ -8,9 +8,15 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 function Product(props) {
 
   var [show,setShow] = useState(false);
+  var [obj, setObj] = useState({});
 
   const handleClose=()=>setShow(false);
-  const handleShow=()=>setShow(true);
+
+  const handleShow=(item)=>{
+      setShow(true);
+      setObj(item);
+      
+  };
 
   return (
     <div className="container">
@@ -18,7 +24,7 @@ function Product(props) {
         <div key={product.id} className="border">
         <p className="h4">{product.desc} 
         <span className="price"><strong>${product.price}</strong></span></p> 
-        <img src={product.image} alt="img" onClick={()=>handleShow} />
+        <img src={product.image} alt="img" onClick={(event)=>handleShow(product)} />
 
         <button onClick={()=>props.addProduct(product)}>
         <FontAwesomeIcon icon={faCirclePlus} style={{color: "#6e91cf",}} />        
@@ -29,18 +35,22 @@ function Product(props) {
         </button>
 
         <input type="number" value={product.value} /> Quantity
-        </div>))}
+       
+        
+      </div>
+      ))}
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>{props.desc}</Modal.Title>
+          <Modal.Title>{obj.desc}</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-          {<img alt="img" src={props.image} width="350" className="mx-5"></img>}
-         <p><span className="text-dark">Ratings: {props.rating}</span></p> 
+          {<img alt="img" src={obj.image} width="350" className="mx-5"></img>}
+         <p><span className="text-dark">Ratings: {obj.rating}</span></p> 
          </Modal.Body>
         
       </Modal>
+      
     
     </div>
   
